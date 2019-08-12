50790c38f096440e3a8b01442d3ecc243c27dc56
ZhaoJun <302563746@qq.com>
2019-08-12 15:07:08 +0800

    49

--- src/jzoffer/Solution49.java
+++ src/jzoffer/Solution49.java
@@ -1,35 +1,54 @@
 package jzoffer;
 
 /**
- * 数组中重复的数字
+ * 把字符串转换成整数
  *
- * 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重
- * 复几次。请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+ * 将一个字符串转换成一个整数(实现Integer.valueOf(string)的功能，但是string不符合数字要求时返回0)，要求不能使用字符串转换整数的
+ * 库函数。 数值为0或者字符串不是一个合法的数值则返回0。
+ *
+ * 输入描述:输入一个字符串,包括数字字母符号,可以为空
+ *
+ * 输出描述:如果是合法的数值表达则返回该数字，否则返回0
+ *
+ * 示例1
+ * 输入
+ * +2147483647
+ *     1a33
+ * 输出
+ * 2147483647
+ *     0
  * @author ZhaoJun
- * @date 2019/8/12 14:39
+ * @date 2019/8/12 14:19
  */
 
 public class Solution49 {
-    // Parameters:
-    //    numbers:     an array of integers
-    //    length:      the length of array numbers
-    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
-    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
-    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
-    // Return value:       true if the input is valid, and there are some duplications in the array number
-    //                     otherwise false
-    public boolean duplicate(int numbers[],int length,int [] duplication) {
-        if (numbers == null) {
-            return false;
+    public static int StrToInt(String str) {
+        if (str == null) {
+            return 0;
         }
-        boolean[] booleans = new boolean[length];
-        for (int i : numbers) {
-            if (booleans[numbers[i]]) {
-                duplication[0] = numbers[i];
-                return true;
+        char[] chars = str.toCharArray();
+        int length = chars.length;
+        boolean lt0 = false;
+        int result = 0;
+        for (int i = 0; i < length; i++) {
+            if (i == 0) {
+                if (chars[i] == '-'){
+                    lt0 = true;
+                } else if (chars[i] >= '0' && chars[i] <= '9') {
+                    result = chars[i] - '0';
+                }
+            } else {
+                if (chars[i] >= '0' && chars[i] <= '9') {
+                    result = result * 10 + (chars[i] - '0');
+                } else {
+                    return 0;
+                }
             }
-            booleans[numbers[i]] = true;
         }
-        return false;
+        return lt0 ? -result : result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StrToInt("+2147483647"));
     }
 }
